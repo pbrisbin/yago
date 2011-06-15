90e90eaf598e6177eccbc6367e4dfebe197356c6
patrick brisbin <pbrisbin@gmail.com>
2011-06-15 17:10:16 -0400

    more realistic example

--- example.sh
+++ example.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 opts='
-simple, m, simple flag
-quiet , q, output less crap          , true
+quiet , q, output less crap
 files , f, list of files to processed, REQUIRED, N, FILE
 size  , s, desired output size\, ok? , 150     , 1, SIZE, n_size
 '
 
-./yago 'testprog' "$opts" "$@" || exit $?
+output="$(./yago 'testprog' "$opts" "$@")" && eval "$output" || exit $?
+
+echo "quiet is $quiet"
+echo "size is ${size[@]}"
+echo "files are ${files[@]}"
+echo "leftover args are ${args[@]}"
+
+exit 0
