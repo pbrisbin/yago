3312e76c63b574c5bf05cf6ceb5013fc1d30de5e
patrick brisbin <pbrisbin@gmail.com>
2011-06-15 13:54:52 -0400

    handle empty lines in opts string; handle last args being optargs

--- example.sh
+++ example.sh
@@ -7,8 +7,4 @@ files , f, list of files to processed, REQUIRED, N, FILE
 size  , s, desired output size\, ok? , 150     , 1, SIZE, n_size
 '
 
-# actual usage:
-#eval "$(yago 'testprog' "$opts" "$@")"
-
-# testing usage:
-./yago 'testprog' "$opts" -f foo bar -s 10 -q some extra args
+./yago 'testprog' "$opts" "$@" || { echo $ERRSTR; exit $ERRNO; }
