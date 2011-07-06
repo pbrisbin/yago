17af795fa0f030b16da9a30cf9b3205aa6c44b90
patrick brisbin <pbrisbin@gmail.com>
2011-07-06 11:45:19 -0400

    update example with new usage

--- example.sh
+++ example.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# setup the options definition
-opts='
-quiet, q, output less crap
-files, f, list of files to processed, REQUIRED, N, FILE
-size , s, desired output size\, ok? , 150     , 1, SIZE, n_size
-'
-
-# source the yago utility
 source ./yago
 
-# parse the options, sets in-script variables
-yago_parse 'testprog' "$opts" "$@"
+yago_parse 'testprog' "$@" << EOF
 
-# print what was done
+  quiet, q, output less crap
+  files, f, list of files to processed, REQUIRED, N, FILE
+  size , s, desired output size\, ok? , 150     , 1, SIZE, n_size
+
+EOF
+
 echo "quiet is          $quiet"
 echo "size is           ${size[@]}"
 echo "files are         ${files[@]}"
 echo "leftover args are ${args[@]}"
-
 exit 0
