fbeacef64bc531e3b62bc0441f37fae0b7504531
patrick brisbin <pbrisbin@gmail.com>
2011-07-06 11:06:57 -0400

    update example script

--- example.sh
+++ example.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# setup the options definition
 opts='
-quiet , q, output less crap
-files , f, list of files to processed, REQUIRED, N, FILE
-size  , s, desired output size\, ok? , 150     , 1, SIZE, n_size
+quiet, q, output less crap
+files, f, list of files to processed, REQUIRED, N, FILE
+size , s, desired output size\, ok? , 150     , 1, SIZE, n_size
 '
 
-output="$(./yago 'testprog' "$opts" "$@")" && eval "$output" || exit $?
+# source the yago utility
+source ./yago
 
-echo "quiet is $quiet"
-echo "size is ${size[@]}"
-echo "files are ${files[@]}"
+# parse the options, sets in-script variables
+yago_parse 'testprog' "$opts" "$@"
+
+# print what was done
+echo "quiet is          $quiet"
+echo "size is           ${size[@]}"
+echo "files are         ${files[@]}"
 echo "leftover args are ${args[@]}"
 
 exit 0
